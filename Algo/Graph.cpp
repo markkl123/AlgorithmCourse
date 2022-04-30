ee781609ba34e0e8cf9a2dda2772a4fa592a1d47
Mark Klaiman <markkl1998@gmail.com>
2022-04-30 10:57:05 +0300

    Real Graph

---
+++ Algo/Graph.cpp
@@ -0,0 +1,23 @@
+#include "Graph.h"
+
+void Graph::MakeEmptyGraph(int n) {
+    //Edge e;
+    //e.paralelEdgeVector->erase(e.paralelEdgeVector->begin() + e.indexParalelEdge);
+}
+bool Graph::IsAdjacent(int u, int v) {
+
+}
+
+vector<Edge> Graph::GetAdjList(int u) {
+
+}
+void Graph::AddEdge(int u, int v, int c) {
+
+}
+void Graph::RemoveEdge(int u, int v) {
+
+    // ListNode* currNode = getArrOfVertex()[u];
+}
+
+
+
