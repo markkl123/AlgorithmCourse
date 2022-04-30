6960096146b2f66a5243ab17c657a72cf40cd65d
Mark Klaiman <markkl1998@gmail.com>
2022-04-30 14:24:23 +0300

    Graph added

--- Algo/Algo.cpp
+++ Algo/Algo.cpp
@@ -1,11 +1,11 @@
 // Algo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-
+#include "Graph.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    Graph gr = 8;
+    cout << "0";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
